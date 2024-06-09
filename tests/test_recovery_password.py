767a90bd4d0b2adefa1945d763b33ff0e7354fb3
Stepan_Y <stepan.yakovlev.b@yandex.ru>
2024-06-10 01:06:44 +0300

    test_recovery_password is ready

--- tests/test_recovery_password.py
+++ tests/test_recovery_password.py
@@ -1,6 +1,3 @@
-
-from locators.password_recovery_page_locators import PasswordRecoveryPageLocators as rpl
-from data.data import UserData as UD
 import allure
 from pages.recovery_password import RecoveryPassword
 
@@ -13,7 +10,7 @@ class TestCheckRecoveryPasswordPage:
         recovery_password = RecoveryPassword(browser)
         recovery_password.click_on_account_entry()
         recovery_password.click_on_recovery_password()
-        assert recovery_password.get_element_text(rpl.CHECK_TEXT_ON_RECOVERY_PASSWORD_PAGE) == 'Восстановление пароля'
+        assert recovery_password.get_text_recovering_password() == 'Восстановление пароля'
 
     @allure.title('Проверка функции: Восстановление пароля')
     @allure.description('ввод почты и клик по кнопке «Восстановить»')
@@ -21,15 +18,16 @@ class TestCheckRecoveryPasswordPage:
         recovery_password = RecoveryPassword(browser)
         recovery_password.click_on_account_entry()
         recovery_password.click_on_recovery_password()
-        recovery_password.fill_field(rpl.INPUT_USER_EMAIL, UD.email)
-        recovery_password.click_on_element(rpl.RECOVERY_BUTTON)
-        assert recovery_password.get_element_text(rpl.CHECK_TEXT_RECOVERY_PASSWORD) == 'Введите код из письма'
+        recovery_password.fill_field_user_email()
+        recovery_password.click_on_recovery_button()
+        assert recovery_password.get_text_write_text_from_email() == 'Введите код из письма'
 
     @allure.title('Проверка функции: Восстановление пароля')
     @allure.description('клик по кнопке показать/скрыть пароль делает поле активным — подсвечивает его')
     def test_click_eye_button(self, browser):
         recovery_password = RecoveryPassword(browser)
         recovery_password.click_on_account_entry()
-        recovery_password.fill_field(rpl.INPUT_PASSWORD_USER, UD.password)
-        recovery_password.click_on_element(rpl.EYE_BUTTON)
-        assert recovery_password.wait_element(rpl.CHECK_ACTIV_EYE_BUTTON) is not None
+        recovery_password.fill_field_user_password()
+        recovery_password.click_on_eye_button()
+        result = recovery_password.check_field_is_active()
+        assert result
